aafbe7d2905604ab1ce51403214faa9cb578567c
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-16 00:46:39 +0000

    local run pre-commit

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -67,7 +67,8 @@ class InjectAttrs(beam.PTransform):
     ) -> beam.PCollection[zarr.storage.FSStore]:
         return pcoll | "Injecting Attributes" >> beam.Map(self._update_zarr_attrs)
 
-def get_pangeo_forge_build_attrs()-> dict[str, Any]:
+
+def get_pangeo_forge_build_attrs() -> dict[str, Any]:
     """Get build information (git hash and time) to add to the recipe output"""
     # Set up injection attributes
     # This is for demonstration purposes only and should be discussed with the broader LEAP/PGF community
@@ -79,11 +80,12 @@ def get_pangeo_forge_build_attrs()-> dict[str, Any]:
 
     git_url_hash = f"{os.environ['GITHUB_SERVER_URL']}/{os.environ['GITHUB_REPOSITORY']}/commit/{os.environ['GITHUB_SHA']}"
     timestamp = datetime.now(timezone.utc).isoformat()
-    
+
     return {
         "pangeo_forge_build_git_hash": git_url_hash,
         "pangeo_forge_build_timestamp": timestamp,
     }
+
 
 # TODO: Both these stages are generally useful. They should at least be in the utils package, maybe in recipes?
 
@@ -99,11 +101,12 @@ def find_recipe_meta(catalog_meta: List[Dict[str, str]], r_id: str) -> Dict[str,
     )
     return None  # Return None if no matching dictionary is found
 
+
 # load the global config values (we will have to decide where these ultimately live)
 catalog_meta = yaml.load(open("feedstock/catalog.yaml"))
 
-print('DETECTING GITHUB ACTIONS RUN')
-if os.getenv('GITHUB_ACTIONS') == 'true':
+print("DETECTING GITHUB ACTIONS RUN")
+if os.getenv("GITHUB_ACTIONS") == "true":
     print("Running inside GitHub Actions.")
 
     # Get final store path from catalog.yaml input
@@ -116,7 +119,7 @@ else:
     target_daily = False
     target_monthly = False
     pgf_build_attrs = {}
-    
+
 print("Final output locations")
 print(f"{target_daily=}")
 print(f"{target_monthly=}")
@@ -125,18 +128,18 @@ print(f"{pgf_build_attrs=}")
 # Common Parameters
 years = range(2001, 2022)
 months = range(1, 13)
-dataset_url = 'https://zenodo.org/record/7761881/files'
+dataset_url = "https://zenodo.org/record/7761881/files"
 
 ## Monthly version
-input_urls_monthly = [f'{dataset_url}/METAFLUX_GPP_RECO_monthly_{y}.nc' for y in years]
+input_urls_monthly = [f"{dataset_url}/METAFLUX_GPP_RECO_monthly_{y}.nc" for y in years]
 
-pattern_monthly = pattern_from_file_sequence(input_urls_monthly, concat_dim='time')
+pattern_monthly = pattern_from_file_sequence(input_urls_monthly, concat_dim="time")
 METAFLUX_GPP_RECO_monthly = (
     beam.Create(pattern_monthly.items())
     | OpenURLWithFSSpec()  # open_kwargs=open_kwargs
     | OpenWithXarray()
     | StoreToZarr(
-        store_name='METAFLUX_GPP_RECO_monthly.zarr',
+        store_name="METAFLUX_GPP_RECO_monthly.zarr",
         combine_dims=pattern_monthly.combine_dim_keys,
     )
     | InjectAttrs(pgf_build_attrs)
@@ -147,15 +150,15 @@ METAFLUX_GPP_RECO_monthly = (
 
 ## daily version
 input_urls_daily = [
-    f'{dataset_url}/METAFLUX_GPP_RECO_daily_{y}{m:02}.nc' for y in years for m in months
+    f"{dataset_url}/METAFLUX_GPP_RECO_daily_{y}{m:02}.nc" for y in years for m in months
 ]
-pattern_daily = pattern_from_file_sequence(input_urls_daily, concat_dim='time')
+pattern_daily = pattern_from_file_sequence(input_urls_daily, concat_dim="time")
 METAFLUX_GPP_RECO_daily = (
     beam.Create(pattern_daily.items())
     | OpenURLWithFSSpec()  # open_kwargs=open_kwargs
     | OpenWithXarray()
     | StoreToZarr(
-        store_name='METAFLUX_GPP_RECO_daily.zarr',
+        store_name="METAFLUX_GPP_RECO_daily.zarr",
         combine_dims=pattern_daily.combine_dim_keys,
     )
     | InjectAttrs(pgf_build_attrs)
